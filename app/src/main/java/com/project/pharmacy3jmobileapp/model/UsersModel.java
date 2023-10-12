48d98f02a79a610532dac2c4802e9a7983f2a400
Apollo Rafael Tejada <zxctejs@gmail.com>
2023-10-13 01:32:44 +0800

    implemented firebase db, login and registration function

---
+++ app/src/main/java/com/project/pharmacy3jmobileapp/model/UsersModel.java
@@ -0,0 +1,27 @@
+package com.project.pharmacy3jmobileapp.model;
+
+public class UsersModel {
+    private String username;
+    private String password;
+
+    public UsersModel(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
